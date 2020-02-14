484d1089491d14b664d69a7d88d974c44f0dce42
Rahal Semu <59530968+Rahela-HYF@users.noreply.github.com>
2020-02-14 23:53:45 +0100

    Update listener.js
    updated

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,19 +1,9 @@
-function calc(){
+/*function calc(){
     var a = parseInt(document.querySelector("#value1").value);
     var b = parseInt(document.querySelector("#value2").value);
     var op = document.querySelector("#operator").value;
-    var Calculate;
-  
-  
-    if (op == "add"){
-      Calculate = a + b;
-    } else if (op == "min"){
-      Calculate = a - b;
-    } else if (op == "div"){
-      Calculate = a / b;
-    }else if (op == "mul"){
-      Calculate = a * b;
-    }
+    var Calculate;*/
+
   
     document.querySelector("#result").innerHTML = Calculate;
-  }
+  //}
